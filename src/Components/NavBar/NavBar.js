4afc0f6b8622e677764b3934ec73c0d2b1226402
kobesparrow <markcollins.md@gmail.com>
2019-07-11 12:56:05 -0600

    Basic header styles complete

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -7,8 +7,8 @@ const NavBar = () => {
   return (
     <header>
       <div className="nav-bar-icons">
-        <img src={natHabIcon} alt="Natural Habitat Adventures icon" />
-        <img src={wwfIcon} alt="WWF icon" />
+        <img src={natHabIcon} className="nat-hab-logo" alt="Natural Habitat Adventures icon" />
+        <img src={wwfIcon} className="wwf-logo" alt="WWF icon" />
       </div>
       <div className="nav-bar-links">
         <h5>HOME</h5>

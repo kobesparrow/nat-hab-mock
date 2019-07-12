0c4859444051ba063d49089bae5ae61720b3f68d
kobesparrow <markcollins.md@gmail.com>
2019-07-12 10:31:53 -0600

    Carousel function actually working

--- src/Components/FeaturedTrips/FeaturedTrips.js
+++ src/Components/FeaturedTrips/FeaturedTrips.js
@@ -6,23 +6,35 @@ class FeaturedTrips extends Component {
   constructor() {
     super()
     this.state = {
-      display: 0,
       left: 9,
-      right: 2
+      display: 0,
+      right: 1
     }
   }
 
   moveLeft = () => {
     if (this.state.display === 0) {
       this.setState({
-        display: 9,
         left: 8,
+        display: 9,
+        right: 1
+      })
+    } else if (this.state.right === 0) {
+      this.setState({
+        left: 8,
+        display: 7,
+        right: 9
+      })
+    } else if (this.state.left === 0) {
+      this.setState({
+        left: 9,
+        display: 0,
         right: 1
       })
     } else {
       this.setState({
-        display: (this.state.display - 1),
         left: (this.state.left - 1) ,
+        display: (this.state.display - 1),
         right: (this.state.right - 1)
       })
     }
@@ -31,14 +43,26 @@ class FeaturedTrips extends Component {
   moveRight = () => {
     if (this.state.display === 9) {
       this.setState({
-        display: 0,
         left: 9,
+        display: 0,
         right: 2
+      })
+    } else if (this.state.left === 9) {
+      this.setState({
+        left: 0,
+        display: 1,
+        right: 2
+      })
+    } else if (this.state.right === 9) {
+      this.setState({
+        left: 8,
+        display: 9,
+        right: 0
       })
     } else {
       this.setState({
-        display: (this.state.display + 1),
         left: (this.state.left + 1),
+        display: (this.state.display + 1),
         right: (this.state.right + 1)
       })
     }
@@ -54,9 +78,11 @@ class FeaturedTrips extends Component {
       <section className="featured-trips">
         <h3>Featured Trips & Destinations</h3>
         <article className="trip-cards"> 
+          <img src={featuredTripData[this.state.left].image} />
           <button onClick={this.moveLeft}>Back</button>
           { featuredTrips[this.state.display] }
           <button onClick={this.moveRight}>Forward</button>
+          <img src={featuredTripData[this.state.right].image} />
         </article>  
       </section>
     )

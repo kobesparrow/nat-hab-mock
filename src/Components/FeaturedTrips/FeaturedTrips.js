19eb3c04c760d2426ee2fe975233d11a523f8d82
kobesparrow <markcollins.md@gmail.com>
2019-07-12 10:03:36 -0600

    Add functionality to carousel buttons

--- src/Components/FeaturedTrips/FeaturedTrips.js
+++ src/Components/FeaturedTrips/FeaturedTrips.js
@@ -1,14 +1,66 @@
-import React from 'react';
+import React, {Component} from 'react';
+import FeaturedTripCard from '../FeaturedTripCard/FeaturedTripCard';
 import { featuredTripData } from '../../utils/featuredTripData';
 
-const FeaturedTrips = () => {
+class FeaturedTrips extends Component {
+  constructor() {
+    super()
+    this.state = {
+      display: 0,
+      left: 9,
+      right: 2
+    }
+  }
 
-  return (
-    <section>
-      <h5>Featured Trips & Destinations</h5>
-      <p>Insert Stateful Component HERE</p>
-    </section>
-  )
+  moveLeft = () => {
+    if (this.state.display === 0) {
+      this.setState({
+        display: 9,
+        left: 8,
+        right: 1
+      })
+    } else {
+      this.setState({
+        display: (this.state.display - 1),
+        left: (this.state.left - 1) ,
+        right: (this.state.right - 1)
+      })
+    }
+  }
+
+  moveRight = () => {
+    if (this.state.display === 9) {
+      this.setState({
+        display: 0,
+        left: 9,
+        right: 2
+      })
+    } else {
+      this.setState({
+        display: (this.state.display + 1),
+        left: (this.state.left + 1),
+        right: (this.state.right + 1)
+      })
+    }
+  }
+
+  render() {
+
+    let featuredTrips = featuredTripData.map(card => {
+      return <FeaturedTripCard {...card} />
+    })
+
+    return (
+      <section className="featured-trips">
+        <h3>Featured Trips & Destinations</h3>
+        <article className="trip-cards"> 
+          <button onClick={this.moveLeft}>Back</button>
+          { featuredTrips[this.state.display] }
+          <button onClick={this.moveRight}>Forward</button>
+        </article>  
+      </section>
+    )
+  }
 }
 
 export default FeaturedTrips;
--- src/utils/featuredTripData.js
+++ src/utils/featuredTripData.js
@@ -80,5 +80,3 @@ export const featuredTripData = [
     additionalDetails: null
   }
 ]
-
-// export default featuredTripData

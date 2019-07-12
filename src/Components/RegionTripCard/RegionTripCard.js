06666ae4450162127f6dc6b09d39ed7a16f3afdf
kobesparrow <markcollins.md@gmail.com>
2019-07-12 07:45:16 -0600

    Regional trips styled

--- src/Components/RegionTripCard/RegionTripCard.js
+++ src/Components/RegionTripCard/RegionTripCard.js
@@ -3,9 +3,13 @@ import React from 'react';
 const RegionTripCard = ({ title, details, image, alt }) => {
 
   return (
-    <article>
-      <img src={image} alt={alt} />
-      <p>{ title }</p>
+    <article className="regional-trip-card">
+      <div className="regional-image-zoom-container">
+        <img src={image} alt={alt} className="regional-trip-image"/>
+      </div>  
+      <div className="regional-trip-title-box">
+        <h6 className="regional-trip-title">{ title }</h6>
+      </div>
       <p>{ details }</p>
     </article>
   )

2a1cfef257fe11a3c1e475a4f3a9db4ae9c36e57
kobesparrow <markcollins.md@gmail.com>
2019-07-14 10:16:35 -0600

    hover effects on footer links

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -12,55 +12,54 @@ const Footer = () => {
       <section className="footer-columns">
         <section className="footer-column">
           <h6>Interests</h6>
-          <p>Specialized Photography Tours</p>
-          <p>Family Adventures</p>
-          <p>Adventure Cruising</p>
-          <p>Bear Viewing</p>
-          <p>Whale Watching</p>
-          <p>Rain Forest Tours</p>
-          <p>Nat Hab's Active Expeditions</p>
-          <p>Hiking & Trekking Adventures</p>
-          <p>Kayaking Adventures</p>
-          <p>Custom African Safari Planner</p>
+          <p>Specialized Photography Tours</p><br />
+          <p>Family Adventures</p><br />
+          <p>Adventure Cruising</p><br />
+          <p>Bear Viewing</p><br />
+          <p>Whale Watching</p><br />
+          <p>Rain Forest Tours</p><br />
+          <p>Nat Hab's Active Expeditions</p><br />
+          <p>Hiking & Trekking Adventures</p><br />
+          <p>Kayaking Adventures</p><br />
+          <p>Custom African Safari Planner</p><br />
         </section>
         <section className="footer-column">
           <h6>Regions</h6>
-          <p>Churchill Polar Bear Tours</p>
-          <p>Galapagos Cruises & Tours</p>
-          <p>African Safaris</p>
-          <p>Alaska & Northern Adventures</p>
-          <p>Antarctica & Arctic Voyages</p>
-          <p>Asia & Pacific Adventures</p>
-          <p>Mexico & Central America Tours</p>
-          <p>South America Adventures</p>
-          <p>U.S. National Parks Tours</p>
-          <p>Europe Adventures</p>
+          <p>Churchill Polar Bear Tours</p><br />
+          <p>Galapagos Cruises & Tours</p><br />
+          <p>African Safaris</p><br />
+          <p>Alaska & Northern Adventures</p><br />
+          <p>Antarctica & Arctic Voyages</p><br />
+          <p>Asia & Pacific Adventures</p><br />
+          <p>Mexico & Central America Tours</p><br />
+          <p>South America Adventures</p><br />
+          <p>U.S. National Parks Tours</p><br />
+          <p>Europe Adventures</p><br />
         </section>
         <section className="footer-column">
           <h6>About Us</h6>
-          <p>Our Trips</p>
-          <p>Why Travel With Us?</p>
-          <p>Guides & Staff</p>
-          <p>Letter from the President</p>
-          <p>Quality & Value Guarantee</p>
-          <p>Sustainability & Conservation</p>
-          <p>Natural Habitat Philanthropy</p>
-          <p>Natural Habitat Philanthropy</p>
-          <p>New & Exploratory Adventures</p>
-          <p>Blogs</p>
-          <p>Contact Us</p>
+          <p>Our Trips</p><br />
+          <p>Why Travel With Us?</p><br />
+          <p>Guides & Staff</p><br />
+          <p>Letter from the President</p><br />
+          <p>Quality & Value Guarantee</p><br />
+          <p>Sustainability & Conservation</p><br />
+          <p>Natural Habitat Philanthropy</p><br />
+          <p>New & Exploratory Adventures</p><br />
+          <p>Blogs</p><br />
+          <p>Contact Us</p><br />
         </section>
         <section className="footer-column">
           <h6>Travel Resources</h6>
-          <p>Trip Forms</p>
-          <p>Nat Hab Gear Store</p>
-          <p>Wildlife & Nature Videos</p>
-          <p>The Habitat Club</p>
-          <p>Important Information</p>
-          <p>Our Commitment to the Climate</p>
-          <p>Webinars</p>
-          <p>Privacy Policy</p>
-          <p>Sitemap</p>
+          <p>Trip Forms</p><br />
+          <p>Nat Hab Gear Store</p><br />
+          <p>Wildlife & Nature Videos</p><br />
+          <p>The Habitat Club</p><br />
+          <p>Important Information</p><br />
+          <p>Our Commitment to the Climate</p><br />
+          <p>Webinars</p><br />
+          <p>Privacy Policy</p><br />
+          <p>Sitemap</p><br />
         </section>
       </section>
       <section className="social-icons">
